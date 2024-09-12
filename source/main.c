150af7ac41bd093d4390f29c79bf07c82e5bbe44
Movie Vertigo <movievertigo@gmail.com>
2024-09-12 11:03:38 +0100

    Interleaved sin/cos table for faster access and added colour

--- source/main.c
+++ source/main.c
@@ -15,15 +15,18 @@
 #define ANG2INC (s32)((CURVESTEP * SINTABLEENTRIES) / (2*PI))
 #define SCALEMUL (s32)(SIZE*PI)
 
-s16 SinTable[SINTABLEENTRIES + SINTABLEENTRIES/4];
-s16* CosTable = SinTable + SINTABLEENTRIES/4;
+s32 SinTable[SINTABLEENTRIES];
 
 void ExpandSinTable()
 {
     for (int i = 0; i < SINTABLEENTRIES/4; ++i)
     {
-        SinTable[i] = SinTable[SINTABLEENTRIES/2 - i - 1] = SinTable[SINTABLEENTRIES + i] = compactsintable[i];
+        SinTable[i] = SinTable[SINTABLEENTRIES/2 - i - 1] = compactsintable[i];
         SinTable[SINTABLEENTRIES/2 + i] = SinTable[SINTABLEENTRIES - i - 1] = -compactsintable[i];
+    }
+    for (int i = 0; i < SINTABLEENTRIES; ++i)
+    {
+        *((s16*)(SinTable+i)+1) = *(s16*)(SinTable+((i+SINTABLEENTRIES/4)%SINTABLEENTRIES));
     }
 }
 
@@ -39,48 +42,52 @@ int main(void)
 
 
 	int bgId = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
-    bool phase = true;
+    bool phase = false;
     u16* buffer1 = bgGetGfxPtr(bgId);
     u16* buffer2 = bgGetGfxPtr(bgId) + 256*256;
-    u16* buffer = phase ? buffer2 : buffer1;
 
     int frame = 0;
     cpuStartTiming(0);
 	while(true)
 	{
+		swiWaitForVBlank();
+        phase = !phase;
+        bgSetMapBase(bgId, phase ? 0 : 8);
+        u16* buffer = phase ? buffer2 : buffer1;
+
         dmaFillWords(0, buffer, SCREENWIDTH*SCREENHEIGHT*2);
 
         s32 time = frame * (1<<SINTABLEPOWER) / 1000;
         s32 ang1Start = time;
         s32 ang2Start = time;
 
+        u16* screenCentre = buffer + (SCREENWIDTH + SCREENWIDTH*SCREENHEIGHT) / 2;
+
         for (int i = 0; i < CURVECOUNT; i += CURVESTEP)
         {
+            const s32 red = (i >> 3)|BIT(15);
             s32 x = 0, y = 0;
             for (int j = 0; j < ITERATIONS; ++j)
             {
-                const s32 sinoffset1 = (ang1Start + x)&(SINTABLEENTRIES-1);
-                const s32 sinoffset2 = (ang2Start + y)&(SINTABLEENTRIES-1);
-                x = SinTable[sinoffset1] + SinTable[sinoffset2];
-                y = CosTable[sinoffset1] + CosTable[sinoffset2];
-                const s32 pX = ((x * SCALEMUL) >> SINTABLEPOWER) + (SCREENWIDTH/2);
-                const s32 pY = ((y * SCALEMUL) >> SINTABLEPOWER) + (SCREENHEIGHT/2);
-                buffer[pY*SCREENWIDTH + pX] = 0xFFFF;
+                const s32 values1 = SinTable[(ang1Start + x)&(SINTABLEENTRIES-1)];
+                const s32 values2 = SinTable[(ang2Start + y)&(SINTABLEENTRIES-1)];
+                x = (s32)(s16)values1 + (s32)(s16)values2;
+                y = (values1>>16) + (values2>>16);
+                const s32 pX = (x * SCALEMUL) >> SINTABLEPOWER;
+                const s32 pY = (y * SCALEMUL) >> SINTABLEPOWER;
+                const s32 green = j >> 3;
+                const s32 blue = (62-(red+green))>>1;
+                screenCentre[pY*SCREENWIDTH + pX] = red + (green<<5) + (blue<<10);
             }
 
             ang1Start += ANG1INC;
             ang2Start += ANG2INC;
         }
 
-		swiWaitForVBlank();
-        phase = !phase;
-        bgSetMapBase(bgId, phase ? 0 : 8);
-        buffer = phase ? buffer2 : buffer1;
-
         ++frame;
         if ((frame%60) == 0)
         {
-            iprintf("%ld\n", cpuGetTiming()/33514/frame);
+            iprintf("%ld\n", (u32)(cpuGetTiming()/33.514f/frame));
         }
 	}
 }
